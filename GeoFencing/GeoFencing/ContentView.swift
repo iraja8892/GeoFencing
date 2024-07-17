d8a3b3465f05f7cd24b3e2be1bcfc3273d184bff
iraja8892 <rajainbam0808@gmail.com>
2024-07-17 16:24:12 -0400

    Initial commit creating project

---
+++ GeoFencing/GeoFencing/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GeoFencing
+//
+//  Created by Raja Inbam on 2024-07-17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GeoFencing/GeoFencing/GeoFencingApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeoFencingApp.swift
+//  GeoFencing
+//
+//  Created by Raja Inbam on 2024-07-17.
+//
+
+import SwiftUI
+
+@main
+struct GeoFencingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
